48498d85e5974676235dc0f69a80a03996461ea3
Supraja <supraja.prasad23@gmail.com>
2019-03-12 21:30:42 +0530

    add

--- tuitions/urls.py
+++ tuitions/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from fees.views import *
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
